c5809a1be1256654527e645e558ffe97555015c9
Andrii Podanenko <podarokua@gmail.com>
2018-04-05 15:41:54 +0300

    Update run8.3.x.sh

--- scripts/patches/run8.3.x.sh
+++ scripts/patches/run8.3.x.sh
@@ -11,7 +11,7 @@ wget https://raw.githubusercontent.com/ymcatwincities/openy-project/8.1.x/script
 sudo cp docroot/core/lib/Drupal/Core/DrupalKernel.php /var/backups/DrupalKernel.php || true
 echo "Checking patch could be applied"
 
-patch -p1 --dry-run < 8.4.x.patch | grep checking | wc -l
+patch -p1 --dry-run < 8.3.x.patch | grep checking | wc -l
 if [[ $(patch -p1 --dry-run < 8.3.x.patch | grep checking | wc -l) = 2 ]]; then
   echo "Patch is correct! OpenY detected correctly! Patching..."
   patch -p1 < 8.3.x.patch
